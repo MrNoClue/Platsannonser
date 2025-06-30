c16e8bd41607ca6483d418200fdf503351323819
MrNoClue <thecforsberg@gmail.com>
2025-06-30 12:17:54 +0200

    Since repository is based on project - the filename should be specified from that - not users wd (MrNoClues).

--- platsannonser_getraw.R
+++ platsannonser_getraw.R
@@ -242,7 +242,10 @@ df <- select(df,
 # OUTPUT
 ## Prepare filename
 datum <- format(Sys.Date(), "%y%m%d")  # Ger t.ex. "250623"
-filnamn <- paste0("Platsannonser/platsannons_wtext_", datum, ".xlsx")
+
+### 2 diffrent file-names for output file
+#filnamn <- paste0("Platsannonser/platsannons_wtext_", datum, ".xlsx")
+filnamn <- "Platsannonser/platsannons_wtext.xlsx" # used in .rmd-files
 
 write_xlsx(df, filnamn)
 
